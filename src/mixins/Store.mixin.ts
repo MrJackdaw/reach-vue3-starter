fb4543fb67b5fcba9dc1b33bc59268f224086810
MrJackdaw <MrJackdaw@users.noreply.github.com>
2022-06-06 07:51:02 -0700

    * updates readme

--- src/mixins/Store.mixin.ts
+++ src/mixins/Store.mixin.ts
@@ -4,11 +4,14 @@ import store from "../state/index";
 const StoreMixin = defineComponent({
   data(): any {
     return {
+      /** Components should override this */
       store: {},
+      /** This will become an "unsubscribe" function when the component mounts */
       unsubscribeStore: null,
     };
   },
 
+  /** Unsubscribe from global state */
   beforeUnmount() {
     if (!this.unsubscribeStore) return;
     this.unsubscribeStore();
@@ -17,10 +20,17 @@ const StoreMixin = defineComponent({
   methods: {
     /** Respond to updates from global store instance */
     onStoreUpdate(updates: any) {
+      // "updates" is a sub-section of the updated global state. It will
+      // contain some or all keys specified by the component that uses this mixin.
       this.store = { ...this.store, ...updates };
     },
 
+    /**
+     * Call this on `mount` to link your component to the global state.
+     * The subscription will trigger when any property in `keys` is updated. */
     subscribe(keys: string[]) {
+      // Subscription returns an "unsubscribe" function (used in unmount).
+      // See docs for @jackcom/raphsducks to see how the global store works.
       this.unsubscribeStore = store.subscribeToKeys(this.onStoreUpdate, keys);
     },
   },
